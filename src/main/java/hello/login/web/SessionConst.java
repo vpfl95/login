762101f4304f37f7046c93da1d660ddbf092da11
vpfl95 <k_dh610@naver.com>
2023-03-30 21:04:55 +0900

    로그인 처리하기 - 서블릿 HTTP 세션2

---
+++ src/main/java/hello/login/web/SessionConst.java
@@ -0,0 +1,9 @@
+package hello.login.web;
+
+public abstract class SessionConst {
+    /**
+     * 생성할 객체가 아니라 static final로 글자 참고하려고 쓰는거라
+     * abstract로 추상클래스로 만들거나 interface로 만든다.
+     */
+    public static final String LOGIN_MEMBER = "loginMember";
+}
